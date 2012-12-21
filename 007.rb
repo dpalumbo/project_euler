fb9f8e2bdc451e98b9efe487a6a1fb1e04362afd
Dan Palumbo <daniel.v.palumbo@gmail.com>
2012-12-21 02:00:22 -0500

    update

---
+++ 007.rb
@@ -0,0 +1,18 @@
+prime = 0
+not_primes = {}
+
+i = 1
+cap = 999999 #determined cap experimentally. just needs to be bigger than the answer
+cnt = 0
+
+while cnt < 10001
+	i+=1
+	if !not_primes.has_key?(i)
+		cnt += 1
+		for j in 1..(cap/i).floor
+			not_primes[j*i]=true
+		end
+	end
+end
+
+puts i
